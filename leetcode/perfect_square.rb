560746004192f0a99d0bf2ef267905139a72b0fd
jkurthoconnor <jkurthoconnor@gmail.com>
2019-05-16 19:38:18 -0600

    Update comments; remove naive implementation

--- leetcode/perfect_square.rb
+++ leetcode/perfect_square.rb
@@ -4,24 +4,7 @@
 # Given a positive integer num, write a function which returns True if num is a perfect square else False.
 
 # Original, Naive Implementation: Use a literal number line to run binary search over
-#
 # TIME: O(N log N)
-# def is_perfect_square(n)
-#   low = 0
-#   high = n / 3
-#   series = (1..high).to_a
-
-#   while low <= high
-#     mid = low + (high - low) / 2
-
-#     square = series[mid] * series[mid]
-#     return true if square == n
-
-#     (square < n) ? (low = mid + 1) : (high = mid - 1)
-#   end
-
-#   return false
-# end
 
 
 
