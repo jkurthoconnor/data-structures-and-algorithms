bafa624f252f24cad5ebec9d0a428e3edf641770
jkurthoconnor <jkurthoconnor@gmail.com>
2019-05-17 21:11:41 -0600

    Refactor: O(N) Time, O(1) Space

--- leetcode/move_zeros.rb
+++ leetcode/move_zeros.rb
@@ -13,34 +13,46 @@
 # ALGO 
 #   MODEL: anchor / runner
 #
-# iterate until find zero
-#  loop from that point until end of array
-#   value at idx = value at idx + 1
-#  array[lastidx] = 0
+#   iterate anchor until find zero
+  # iterate runner constantly
+#
+#  when a non-zero source is found beyond the zero, and anchor is at a zero value
+#   value at anchor takes value at source
+#   value at source becomes zero
 
 
+# TIME: O(N) SPACE: O(1)
+# LeetCode: faster than 98%; less memory than 99%
 def move_zeroes(nums)
-  reader = 0
-  del_count = 0
+  z_finder = 0
+  source = 0
 
-  while reader < nums.length - del_count
-    if nums[reader].zero?
-      nums.delete_at(reader) # MAGIC!!!
-      del_count += 1
-      nums[nums.length] = 0
-    else
-      reader += 1
+  while source < nums.length && z_finder < nums.length
+    if nums[z_finder].zero? && !nums[source].zero?
+      nums[z_finder] = nums[source]
+      nums[source] = 0
     end
+
+    if !nums[z_finder].zero?
+      z_finder += 1 
+    end
+
+    source += 1 
   end
 end
-a1 = [0,1,0,3,12]  
+
+
+a1 = [0,1,0,3,12]
 a2 = [9,1,0,3,13,12,0]
 a3 = [0,1,0,3,0,12,0]
+a4 = [0,0,0,0,0,12,0]
 
-move_zeros(a1)
-move_zeros(a2)
-move_zeros(a3)
+move_zeroes(a1)
+move_zeroes(a2)
+move_zeroes(a3)
+move_zeroes(a4)
 
 p a1
 p a2
 p a3
+p a4
