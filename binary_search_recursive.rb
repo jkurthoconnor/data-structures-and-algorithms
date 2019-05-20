d120ad393769881e0b23d57374279e52c80a016d
jkurthoconnor <jkurthoconnor@gmail.com>
2019-05-20 16:03:39 -0600

    Make base case more explict

--- binary_search_recursive.rb
+++ binary_search_recursive.rb
@@ -3,20 +3,18 @@ def binary_search(array, target)
 end
 
 def binary_search_helper(arr, target, low, high)
+  return - 1 if low > high
+
   mid = low + (high - low) / 2
+  mid_value = arr[mid]
 
-  while low <= high
-    mid_value = arr[mid]
-    return mid if mid_value == target
-
-    if mid_value < target
-      return binary_search_helper(arr, target, mid + 1, high)
-    else 
-      return binary_search_helper(arr, target, low, mid - 1)
-    end
+  if mid_value == target
+    return mid
+  elsif mid_value < target
+    return binary_search_helper(arr, target, mid + 1, high)
+  else 
+    return binary_search_helper(arr, target, low, mid - 1)
   end
-
-  return -1
 end
 
 p binary_search([1,2,3,4,5], 5) # 4
