94af63783bf834b6589c58d2e27b5d27b007e6ca
jkurthoconnor <jkurthoconnor@gmail.com>
2019-05-20 18:32:16 -0600

    Remove tests to use in require statement

--- binary_search_recursive.rb
+++ binary_search_recursive.rb
@@ -17,10 +17,10 @@ def binary_search_helper(arr, target, low, high)
   end
 end
 
-p binary_search([1,2,3,4,5], 5) # 4
-p binary_search([-10,-8,-3,-2,-1], -1) # 4
-p binary_search([1,2,3,4,5], 3) # 2
-p binary_search([1,2,3,4,5], 2) # 1
-p binary_search([-10,-2,3,4,5], -2) # 1
-p binary_search([1,2,3,4,5], 1) # 0
-p binary_search([1,2,3,4,5], 8) # -1
+# p binary_search([1,2,3,4,5], 5) # 4
+# p binary_search([-10,-8,-3,-2,-1], -1) # 4
+# p binary_search([1,2,3,4,5], 3) # 2
+# p binary_search([1,2,3,4,5], 2) # 1
+# p binary_search([-10,-2,3,4,5], -2) # 1
+# p binary_search([1,2,3,4,5], 1) # 0
+# p binary_search([1,2,3,4,5], 8) # -1
