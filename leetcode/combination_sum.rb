e8737fffc5a2f488a57556f774fee8e6806e1ef2
jkurthoconnor <jkurthoconnor@gmail.com>
2019-06-03 21:28:43 -0600

    Init commit solution

--- leetcode/combination_sum.rb
+++ leetcode/combination_sum.rb
@@ -33,6 +33,49 @@ A solution set is:
 IN: arr uniq int ; target int
 OUT: 2-d array of all possible combinations of input arr values
 
+ALGORITHM:
+
+treat as a combinations problem, with base case managing sums
+
+BASE CASE:
+  if sum of combination == target push target to results
+  elsif sum of combination > target, return
+    
+
+RECURSIVE CASE:
+  for each element in collection 
+    push element onto tmp
+    recurse()
+    pop element off of collection
 
 
 =end
+
+# LeetCode: faster than 6%; less memory than 42%
+def combination_sum(nums, target)
+  result = []
+  helper(nums, target, result)
+  result
+end
+
+def helper(nums, target, result, tmp = [])
+  sum = tmp.sum
+
+  if (sum == target) && !(result.map(&:sort).include?(tmp.sort))
+    result.push(tmp.clone)
+  elsif sum > target
+    return
+  else
+    nums.each do |n|
+      tmp.push(n)
+      helper(nums, target, result, tmp)
+      tmp.pop
+    end
+  end
+end
+
+a1 = [2,3,6,7]
+a2 = [2,3,5]
+
+p combination_sum(a1, 7)
+p combination_sum(a2, 8)
